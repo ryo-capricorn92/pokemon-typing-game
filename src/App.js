63d1d493d2b2343e88dc8d439bdedafcea77925c
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-06-16 13:29:02 -0400

    create basic foundation with create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Grid } from './Presentation'
+
+class App extends Component {
+  render() {
+    console.log(Grid);
+    return (
+      <div className="App">
+        <p>Blegh</p>
+      </div>
+    );
+  }
+}
+
+export default App;
