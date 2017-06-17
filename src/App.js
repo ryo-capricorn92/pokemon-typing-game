73af3d07a58d58cbaa39ef3f20dbd1cc4a9d5988
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-06-16 21:48:43 -0400

    remove original grid testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Grid, Container } from './Presentation'
+import { Container } from './Presentation'
 
 class App extends Component {
   render() {
-    console.log(Grid);
     return (
       <Container>
         <p>Blegh</p>
